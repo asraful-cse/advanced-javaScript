3a9d9b22aa004abf2cb203b2bf58cd5dc8da527d
asraful196 <ahamedsunny14@gmail.com>
2021-03-06 09:27:21 +0600

    object property using map,filter,find

---
+++ object-property.js
@@ -0,0 +1,23 @@
+const students = [
+    {id: 21, name:"Deepjol"},
+    {id: 32, name:'Fahim'},
+    {id: 51, name:'soyeb'},
+    {id: 71, name:'rasel'}
+]
+
+const names = students.map(s => s.name);
+ console.log(names);    // output:[ 'Deepjol', 'Fahim', 'soyeb', 'rasel' ]
+
+const ids = students.map(s => s.id );
+ console.log(ids);       //output: [ 21, 32, 51, 71 ]
+
+const bigger = students.filter(s => s.id>30);
+ console.log(bigger);
+//output::
+// [
+//     { id: 32, name: 'Fahim' },
+//     { id: 51, name: 'soyeb' },
+//     { id: 71, name: 'rasel' }
+//   ]
+const biggerOne = students.find(s => s.id>30);
+console.log(biggerOne);    // output:: { id: 32, name: 'Fahim' }
