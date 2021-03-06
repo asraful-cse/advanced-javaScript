fd0e25a4253c7636830e480113bd2c5d324f9661
asraful196 <ahamedsunny14@gmail.com>
2021-03-06 10:19:47 +0600

    scope ,block scope ,access other scope variable

---
+++ scope.js
@@ -0,0 +1,15 @@
+// :23-7:scope ,block scope ,access other scope variable..
+
+
+// let and const::ei doita hosse scope variable oi {}/backet er vetore thake 
+// tader ager position jei khane ase tar opore jabe na.
+
+
+// var:: eita hosse onek flexible .. and
+//  eita abar kico kico khetre confuse kore fele.
+// jemon eita error dekhabe na bahir theke jodi console.log() kora hoi,
+// eita parent ke dore var ke output hisebe undefined / ja ase ta dekhai.
+
+
+//  https://www.thatjsdude.com/ > JS : Scope and Closure...
+// Understand JavaScript scope, hoisting, closure with performance secret.
